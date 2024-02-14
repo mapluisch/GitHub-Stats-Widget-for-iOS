14d4d11cca04d85b6084b7033a5140724a6e836d
Martin Pluisch <31780571+mapluisch@users.noreply.github.com>
2024-02-14 14:55:49 +0100

    Swift BP refactoring

--- WidgetExtension/WidgetView.swift
+++ WidgetExtension/WidgetView.swift
@@ -8,56 +8,71 @@
 import SwiftUI
 import WidgetKit
 
+// MARK: - GitHubStatsWidgetView
+
 struct GitHubStatsWidgetView: View {
     let entry: GitHubUserStatsEntry
     @Environment(\.colorScheme) var colorScheme
     
     var body: some View {
         VStack(alignment: .leading, spacing: 16) {
-            HStack {
-                Image(colorScheme == .dark ? "github-light" : "github-dark")
-                    .renderingMode(.original)
-                        .resizable()
-                        .scaledToFit()
-                        .frame(width: 24, height: 24)
-                Text(entry.username)
-                    .font(.headline)
-                    .minimumScaleFactor(0.5)
-                    .lineLimit(4)
-                    .frame(maxWidth: .infinity, alignment: .leading)
-            }
-            VStack(alignment: .leading, spacing: 6) {
-                if entry.configuration.useIcons as? Bool ?? false {
-                    HStack {
-                        Image(colorScheme == .dark ? "followers-light" : "followers-dark")
-                            .renderingMode(.original)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20, height: 20)
-                        Text("\(entry.followers)")
-                    }
-                    HStack {
-                        Image(colorScheme == .dark ? "star-light" : "star-dark")
-                            .renderingMode(.original)
-                                .resizable()
-                                .scaledToFit()
-                                .frame(width: 20, height: 20)
-                        Text("\(entry.stars)")
-                    }
-                } else {
-                    Text("Followers: \(entry.followers)")
-                    Text("Stars: \(entry.stars)")
-                }
-            }
+            userInfo
+            statsInfo
         }
         .containerBackground(Color(UIColor.systemBackground), for: .widget)
     }
+    
+    // MARK: - Subviews
+    
+    private var userInfo: some View {
+        HStack {
+            iconImage(named: colorScheme == .dark ? "github-light" : "github-dark", width: 24, height: 24)
+            Text(entry.username)
+                .font(.headline)
+                .minimumScaleFactor(0.5)
+                .lineLimit(4)
+                .frame(height: 24, alignment: .center)
+        }
+    }
+    
+    private var statsInfo: some View {
+        VStack {
+            if entry.configuration.useIcons as? Bool ?? true {
+                iconAndText(for: "followers", count: entry.followers)
+                iconAndText(for: "star", count: entry.stars)
+            } else {
+                Text("Followers: \(entry.followers)")
+                Text("Stars: \(entry.stars)")
+            }
+        }
+    }
+    
+    // MARK: - Helper Functions
+    
+    private func iconAndText(for type: String, count: Int) -> some View {
+        let iconName = "\(type)-\(colorScheme == .dark ? "light" : "dark")"
+        return HStack {
+            iconImage(named: iconName, width: 20, height: 20)
+            Text("\(count)")
+                .minimumScaleFactor(0.5)
+                .lineLimit(1)
+        }
+    }
+    
+    private func iconImage(named name: String, width: CGFloat, height: CGFloat) -> some View {
+        Image(name)
+            .renderingMode(.original)
+            .resizable()
+            .scaledToFit()
+            .frame(width: width, height: height)
+    }
 }
 
+// MARK: - Previews
 
 struct GitHubStatsWidgetView_Previews: PreviewProvider {
     static var previews: some View {
-        GitHubStatsWidgetView(entry: GitHubUserStatsEntry(date: Date(), username: "mapluisch", followers: 0, stars: 0, configuration: GitHubUserConfigurationIntent()))
+        GitHubStatsWidgetView(entry: GitHubUserStatsEntry(date: Date(), username: "mapluisch", followers: 12, stars: 34, configuration: GitHubUserConfigurationIntent()))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
